c4ba286b1e2f0abbfe47e7c062c7a8e64f03cc2e
elianaalonso <elianaalonsocastro@gmail.com>
2024-08-18 20:29:15 -0300

    click solucionado

--- eventos.js
+++ eventos.js
@@ -1,3 +1,16 @@
 document.querySelector('div').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+
+const miBoton = document.getElementById('miBoton');
+const miDiv = document.getElementById('miDiv');
+
+
+miBoton.addEventListener('click', function(event) {
+  event.stopPropagation(); 
+});
+
+
+
+  
